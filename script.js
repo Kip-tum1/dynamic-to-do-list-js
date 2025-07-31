80ed6799f5ebb2fee61bdf3018c45a9d9473d5e8
Kiptum <duncankipkoech12@gmail.com>
2025-07-31 19:46:23 +0300

    todo-list

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask(){
+        let taskText = taskInput.value;
+        if(taskText !== ""){
+            let listElement= document.createElement("li");
+            listElement.textContent = taskText;
+
+            let button = document.createElement("button");
+            button.textContent = "Remove";
+            button.classList.add("remove-btn");
+
+            button.addEventListener("click", function (event){
+                if(event.target.tagName === "BUTTON"){
+                    event.target.parentElement.remove();                   
+                }
+                
+            }) 
+
+
+            listElement.appendChild(button);
+            taskList.appendChild(listElement);
+
+            taskInput.value = ""
+
+            
+            
+            
+        }
+        else{
+            alert("Enter a task")
+            
+        }
+
+        
+
+
+        
+        
+
+        
+    }
+
+    addButton.addEventListener("click", addTask);
+
+    taskInput.addEventListener("keypress", function(event){
+        if(event.key === "Enter"){
+            addTask();
+        }
+    } )
+
+    
+
+})
